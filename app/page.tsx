0940c81d073af551aa4595b749782db61ee249ac
testgithubritgu <swapnilraut1698@gmail.com>
2026-02-01 11:40:09 +0530

    initiliaze next app

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <Image
           className="dark:invert"
           src="/next.svg"
-          alt="Next.js logo"
+          alt="Next.js logo" 
           width={100}
           height={20}
           priority
